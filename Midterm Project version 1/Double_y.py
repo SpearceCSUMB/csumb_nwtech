4dab054b98b1edec33a2077de3a208eb76d77d24
sheridanb2008 <31253905+sheridanb2008@users.noreply.github.com>
2018-11-17 00:41:29 -0800

    Update Midterm Project version 1/Double_y.py

--- Midterm Project version 1/Double_y.py
+++ Midterm Project version 1/Double_y.py
@@ -1,18 +1,27 @@
+# Brian Sheridan And Kevin Bentley
+# CST 205 
+# Midterm Project Double-y
 
 def doubley(source):
+# CREATE TH ROOT PATH
   root = os.path.dirname(os.path.realpath(__file__))
   art = root = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Clipart")
+# BRING THE IMAGES INTO THE PROGRAM  
   pic1 = makePicture(source)
   print "Choose another picture to combined with the original." 
   pic2 = makePicture(pickAFile())
+# CREATE NEW PICTURE TO ALLOW FOR MULTIPY IMAGAE SIZES
   pic3 = makeEmptyPicture(min(getWidth(pic1),getWidth(pic2)),min(getHeight(pic1),getHeight(pic2)))
   pic4 = makePicture(os.path.join(root,"4.jpg"))  
+# FUNCTIONS
   combine(pic3,pic1,pic2)
   greenScreen(pic3,pic4,(getWidth(pic3) - getWidth(pic4)),(getHeight(pic3) - getHeight(pic4)))
   show(pic3)
   
 def combine(output, source1, source2):
   x1, x2, y1, y2 = 0, 0, 0, 0
+# SET THE STARTING POINT TO OVERLAY IMAGE
+# THE SIDES WITH THE LEAST PIXELS WILL BE SELECTED 
    
   if getWidth(source1) > getWidth(source2):
     x1 = getWidth(source1) - getWidth(source2)
@@ -22,6 +31,7 @@ def combine(output, source1, source2):
     y1 = getHeight(source1) - getHeight(source2)
   elif getHeight(source1) < getHeight(source2):
     y2 = getHeight(source2) - getHeight(source1)    
+# OVERLAY IMAGES AND AVERAGE PIXELS TOGETHER
   for x in range(0, getWidth(output)):
     for y in range(0, getHeight(output)):
       pix1 = getPixel(source1, x + (x1 / 2), y + (y1 / 2))
